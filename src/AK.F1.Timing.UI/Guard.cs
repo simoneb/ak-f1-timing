00728da49cceecef94effbd713ea11b9d7de7445
andrew.kernahan <andrew.kernahan@3a0ce762-4747-11df-839d-f73fd85d8354>
2009-10-17 20:11:06 +0000

    Initial commit.

---
+++ src/AK.F1.Timing.UI/Guard.cs
@@ -0,0 +1,74 @@
+﻿// Copyright 2009 Andy Kernahan
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Diagnostics;
+
+namespace AK.F1.Timing.UI
+{
+    /// <summary>
+    /// Library guard. This class is <see langword="static"/>.
+    /// </summary>
+    internal static class Guard
+    {
+        #region Internal Interface.
+
+        [DebuggerStepThrough]
+        internal static void NotNull<T>(T instance, string paramName) where T: class {
+
+            if(instance == null) {
+                throw new ArgumentNullException(paramName);
+            }
+        }
+
+        [DebuggerStepThrough]
+        internal static void InRange(bool condition, string paramName) {
+
+            if(!condition) {
+                throw ArgumentOutOfRange(paramName);
+            }
+        }
+
+        [DebuggerStepThrough]
+        internal static void NotNullOrEmpty(string s, string paramName) {
+
+            NotNull(s, paramName);
+            if(s.Length == 0) {
+                throw new ArgumentException(Resource.ArgEmptyString, paramName);
+            }
+        }
+
+        internal static void ObjectDisposed(object instance) {
+
+            throw new ObjectDisposedException(instance.GetType().Name);
+        }
+
+        internal static ArgumentOutOfRangeException ArgumentOutOfRange(string paramName) {
+
+            return new ArgumentOutOfRangeException(paramName);
+        }
+
+        internal static NotImplementedException NotImplemented() {
+
+            return new NotImplementedException();
+        }
+
+        private static string Format(string format, params object[] args) {
+
+            return string.Format(Resource.Culture, format, args);
+        }
+
+        #endregion
+    }
+}
