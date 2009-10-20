a25f93a38a28417eccb707b967a39103e23b0ab0
andrew.kernahan <andrew.kernahan@3a0ce762-4747-11df-839d-f73fd85d8354>
2009-10-20 19:35:44 +0000

    Initial commit.

---
+++ src/AK.F1.Timing.Model/Driver/PitTimesModel.cs
@@ -0,0 +1,57 @@
+﻿// Copyright 2009 Andy Kernahan
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.ObjectModel;
+
+namespace AK.F1.Timing.Model.Driver
+{
+    /// <summary>
+    /// Contains information relating to a pit time.
+    /// </summary>
+    [Serializable]
+    public class PitTimesModel
+    {       
+        #region Public Interface.
+
+        /// <summary>
+        /// Initialises a new instance of the <see cref="PitTimesModel"/> class.
+        /// </summary>
+        /// <param name="time">The pit time. Note: this time includes the time taken to travel
+        /// the pit lane.</param>
+        /// <param name="lapNumber">The lap number on which the pit time was set.</param>
+        public PitTimesModel() { }
+
+        /// <summary>
+        /// Adds the specified item to this collection.
+        /// </summary>
+        /// <param name="item">The item to add to this collection.</param>
+        /// <exception cref="System.ArgumentNullException">
+        /// Thrown when <paramref name="item"/> is <see langword="null"/>.
+        /// </exception>
+        public void Add(PitTimeModel item) {
+
+            Guard.NotNull(item, "item");
+
+            this.Values.Add(item);
+        }
+
+        /// <summary>
+        /// Gets the collection of <see cref="PitTimeModel"/>s.
+        /// </summary>
+        public ObservableCollection<PitTimeModel> Values { get; private set; }        
+
+        #endregion
+    }
+}
