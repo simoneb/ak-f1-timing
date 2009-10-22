50f4707740944568357e3a75be27b836fe1a9fc7
andrew.kernahan <andrew.kernahan@3a0ce762-4747-11df-839d-f73fd85d8354>
2009-10-22 18:44:37 +0000

    Added version add-in and configuration.

--- src/AK.F1.Timing.LiveRecorder/Properties/AssemblyInfo.cs
+++ src/AK.F1.Timing.LiveRecorder/Properties/AssemblyInfo.cs
@@ -1,36 +1,34 @@
-﻿using System.Reflection;
+﻿// Copyright 2009 Andy Kernahan
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Resources;
+using System.Reflection;
+using System.Security.Permissions;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("AK.F1.Timing.LiveRecorder")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("360-Systems")]
+[assembly: AssemblyTitle("F1 Live Timing Recorder for .NET 3.5")]
+[assembly: AssemblyDescription("F1 Live Timing Recorder App for .NET 3.5.")]
+[assembly: AssemblyCompany("Andy Kernahan")]
 [assembly: AssemblyProduct("AK.F1.Timing.LiveRecorder")]
-[assembly: AssemblyCopyright("Copyright © 360-Systems 2009")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright © Andy Kernahan 2009")]
+[assembly: AssemblyTrademark("Andy Kernahan")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
+[assembly: NeutralResourcesLanguage("en-GB")]
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("823e7595-dba0-4ce4-a900-71f67086977a")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: CLSCompliant(true)]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum)]
+[assembly: AssemblyVersion("0.1.10.34295")]
+[assembly: AssemblyFileVersion("0.1.10.34295")]
--- src/AK.F1.Timing/Properties/AssemblyInfo.cs
+++ src/AK.F1.Timing/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
 [assembly: SecurityPermission(SecurityAction.RequestMinimum)]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.8.1.34295")]
+[assembly: AssemblyFileVersion("0.8.1.34295")]
