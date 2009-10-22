a4b90d89847fb8a6fa7f760047952f7b8890e53a
andrew.kernahan <andrew.kernahan@3a0ce762-4747-11df-839d-f73fd85d8354>
2009-10-22 18:10:00 +0000

    Iniitial commit.

---
+++ src/AK.F1.Timing/Messaging/Messages/Weather/SetIsWetMessage.cs
@@ -0,0 +1,62 @@
+﻿// Copyright 2009 Andy Kernahan
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using AK.F1.Timing.Messaging.Serialization;
+
+namespace AK.F1.Timing.Messaging.Messages.Weather
+{
+    /// <summary>
+    /// A message which specifies whether the track is dry or wet. This class is
+    /// <see langword="sealed"/>.
+    /// </summary>
+    [Serializable]
+    [TypeId(72954222)]
+    public sealed class SetIsWetMessage : Message
+    {
+        #region Public Interface.
+
+        /// <summary>
+        /// Initialises a new instance of the <see cref="SetWetDryMessage"/> class and specifies
+        /// whether the track is wet or dry.
+        /// </summary>
+        /// <param name="isWet"></param>
+        public SetIsWetMessage(bool isWet) {            
+
+            this.IsWet = isWet;            
+        }
+
+        /// <inheritdoc />
+        public override void Accept(IMessageVisitor visitor) {
+
+            Guard.NotNull(visitor, "visitor");
+
+            visitor.Visit(this);
+        }
+
+        /// <inheritdoc />
+        public override string ToString() {
+
+            return Repr("IsWet='{0}'", this.IsWet);
+        }
+
+        /// <summary>
+        /// Gets a value indicating if the track is wet.
+        /// </summary>
+        [PropertyId(0)]
+        public bool IsWet { get; private set; }
+
+        #endregion
+    }
+}
