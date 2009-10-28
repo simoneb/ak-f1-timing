f839b8822d45fe44530ffc176da1f569f5d3d759
andrew.kernahan <andrew.kernahan@3a0ce762-4747-11df-839d-f73fd85d8354>
2009-10-28 19:33:08 +0000

    Initial commit.

---
+++ src/AK.F1.Timing.Model/Driver/LapHistoryEntry.cs
@@ -0,0 +1,82 @@
+﻿// Copyright 2009 Andy Kernahan
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+
+using AK.F1.Timing.Messaging.Messages.Driver;
+
+namespace AK.F1.Timing.Model.Driver
+{
+    /// <summary>
+    /// Defines the times posted during a complete lap.
+    /// </summary>
+    [Serializable]
+    public class LapHistoryEntry
+    {
+        #region Public Interface.
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="s1">The sector one time set during the lap.</param>
+        /// <param name="s2">The sector two time set during the lap.</param>
+        /// <param name="s3">The sector three time set during the lap.</param>
+        /// <param name="lap">The overall lap time set</param>
+        /// <param name="pitted"><see langword="true"/> if the driver pitted at the end of the lap,
+        /// otherwise; <see langword="false"/>.</param>
+        public LapHistoryEntry(PostedTime s1, PostedTime s2, PostedTime s3, PostedTime lap, bool pitted) {
+
+            this.S1 = s1;
+            this.S2 = s2;
+            this.S3 = s3;
+            this.Lap = lap;
+            this.Pitted = pitted;
+        }
+
+        /// <summary>
+        /// Gets the lap number of this entry.
+        /// </summary>
+        public int LapNumber {
+
+            get { return (this.S1 ?? this.S2 ?? this.S3).LapNumber; }
+        }
+
+        /// <summary>
+        /// Gets the sector one time set during the lap.
+        /// </summary>
+        public PostedTime S1 { get; private set; }
+
+        /// <summary>
+        /// Gets the sector two time set during the lap.
+        /// </summary>
+        public PostedTime S2 { get; private set; }
+
+        /// <summary>
+        /// Gets the sector three time set during the lap.
+        /// </summary>
+        public PostedTime S3 { get; private set; }
+
+        /// <summary>
+        /// Gets the overall lap time set. This can be null if the driver pitted.
+        /// </summary>
+        public PostedTime Lap { get; private set; }
+
+        /// <summary>
+        /// Gets a value indicating if the driver pitted during this lap.
+        /// </summary>
+        public bool Pitted { get; private set; }
+
+        #endregion
+    }
+}
