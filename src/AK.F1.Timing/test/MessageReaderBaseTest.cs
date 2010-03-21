e30ba58ba20c886ce9c1efb2084c46d5159188fd
andrew.kernahan <andrew.kernahan@3a0ce762-4747-11df-839d-f73fd85d8354>
2010-03-21 12:16:11 +0000

    Added tested for invariants enforced by Read.

--- src/AK.F1.Timing/test/MessageReaderBaseTest.cs
+++ src/AK.F1.Timing/test/MessageReaderBaseTest.cs
@@ -13,13 +13,109 @@
 // limitations under the License.
 
 using System;
+using System.Collections.Generic;
+using System.IO;
 using Xunit;
+
+using AK.F1.Timing.Messages.Weather;
 
 namespace AK.F1.Timing
 {
-
-
     public class MessageReaderBaseTest
     {
+        [Fact]
+        public void read_impl_should_not_be_invoked_again_having_previously_thrown_an_exception_and_same_exception_should_be_re_thrown() {
+
+            var reader = new ThrowingMessageReader();
+
+            Assert.Throws<IOException>(() => reader.Read());
+            Assert.Throws<IOException>(() => reader.Read());
+        }
+
+        [Fact]
+        public void read_impl_should_not_be_invoked_again_having_previously_returned_null() {
+
+            var reader = new NullMessageReader();
+
+            Assert.Null(reader.Read());
+            Assert.Null(reader.Read());
+        }
+
+        [Fact]
+        public void read_impl_should_be_invoked_again_have_previously_returned_an_empty_message() {
+
+            var expected = new SetIsWetMessage(true);
+            var reader = new StubMessageReader(Message.Empty, Message.Empty, expected);
+
+            Assert.Same(expected, reader.Read());
+        }
+
+        [Fact]
+        public void read_should_throw_when_it_has_been_reader_has_been_disposed() {
+
+            var reader = new StubMessageReader();
+
+            reader.Dispose();
+
+            Assert.Throws<ObjectDisposedException>(() => reader.Read());
+        }
+
+        [Fact]
+        public void is_disposed_should_reflect_the_disposed_state_of_the_reader() {
+
+            var reader = new StubMessageReader();
+
+            Assert.False(reader.IsDisposed);
+            reader.Dispose();
+            Assert.True(reader.IsDisposed);
+        }
+
+        private sealed class StubMessageReader : MessageReaderBase
+        {
+            private Queue<Message> _messages;
+
+            public StubMessageReader(params Message[] messages) {
+
+                _messages = new Queue<Message>(messages);
+            }
+
+            public void Dispose() {
+
+                ((IDisposable)this).Dispose();
+            }
+
+            protected override Message ReadImpl() {
+
+                return _messages.Count > 0 ? _messages.Dequeue() : null;
+            }
+        }
+
+        private sealed class ThrowingMessageReader : MessageReaderBase
+        {
+            private bool _readImplCalled = false;
+
+            protected override Message ReadImpl() {
+
+                Assert.False(_readImplCalled, "ReadImpl has already been called.");
+
+                _readImplCalled = true;
+
+                throw new IOException();
+            }
+        }
+
+        private sealed class NullMessageReader : MessageReaderBase
+        {
+            private bool _readImplCalled = false;
+
+            protected override Message ReadImpl() {
+
+                Assert.False(_readImplCalled, "ReadImpl has already been called.");
+
+                _readImplCalled = true;
+
+                return null;
+            }
+        }
     }
 }
