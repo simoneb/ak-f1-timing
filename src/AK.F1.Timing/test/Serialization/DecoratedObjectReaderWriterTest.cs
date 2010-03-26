6f07b37a936d540b0794e20c599dbf9badd3d6a7
andrew.kernahan <andrew.kernahan@3a0ce762-4747-11df-839d-f73fd85d8354>
2010-03-26 21:00:59 +0000

    Added simple complex object tests.

--- src/AK.F1.Timing/test/Serialization/DecoratedObjectReaderWriterTest.cs
+++ src/AK.F1.Timing/test/Serialization/DecoratedObjectReaderWriterTest.cs
@@ -13,8 +13,11 @@
 // limitations under the License.
 
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.IO;
+using System.Runtime.Serialization;
 using Xunit;
 using Xunit.Extensions;
 
@@ -22,7 +25,7 @@ namespace AK.F1.Timing.Serialization
 {
     public class DecoratedObjectReaderWriterTest
     {
-        [Theory(Skip = "BUG: writer should not compress root primitives")]
+        [Theory]
         [ClassData(typeof(PrimitiveDataProvider))]
         public void can_write_and_read_primitives(object primitive) {
 
@@ -30,9 +33,41 @@ namespace AK.F1.Timing.Serialization
         }
 
         [Fact]
+        public void can_write_and_read_multiple_primitives() {
+
+            object[] graphs = new PrimitiveDataProvider().Select(x => x[0]).ToArray();
+
+            using(var stream = new MemoryStream()) {
+                WriteGraphs(stream, graphs);
+                stream.Position = 0L;
+                using(var reader = new DecoratedObjectReader(stream)) {
+                    foreach(var graph in graphs) {
+                        Assert.Equal(graph, reader.Read());
+                    }
+                }
+            }
+        }
+
+        [Theory]
+        [ClassData(typeof(ComplexDataProvider))]
+        public void can_write_and_read_complex_types(object complex) {
+
+            WriteAndRead(complex);
+        }
+
+        [Fact]
         public void graphs_which_implement_object_reference_are_respected() {
 
-            Assert.Same(DBNull.Value, WriteAndRead(DBNull.Value));
+            Assert.Same(ObjectReference.Instance, WriteAndRead(ObjectReference.Instance));
+        }
+
+        private static void WriteGraphs(Stream stream, params object[] graphs) {
+
+            using(var writer = new DecoratedObjectWriter(stream)) {
+                foreach(var graph in graphs) {
+                    writer.Write(graph);
+                }
+            }
         }
 
         private static object WriteAndRead(object graph) {
@@ -40,9 +75,7 @@ namespace AK.F1.Timing.Serialization
             object actual;
 
             using(var stream = new MemoryStream()) {
-                using(var writer = new DecoratedObjectWriter(stream)) {
-                    writer.Write(graph);
-                }
+                WriteGraphs(stream, graph);
                 Assert.NotEqual(0L, stream.Position);
                 stream.Position = 0L;
                 using(var reader = new DecoratedObjectReader(stream)) {
@@ -60,85 +93,180 @@ namespace AK.F1.Timing.Serialization
             return actual;
         }
 
-        public class PrimitiveDataProvider : IEnumerable<object[]>
+        public class ComplexDataProvider : IEnumerable<object[]>
         {
             public IEnumerator<object[]> GetEnumerator() {
 
-                yield return V(null);
-                yield return V('c');
-                yield return V("s");
-                yield return V(true);
-                // Byte
-                yield return V((Byte)0);
-                yield return V(Byte.MinValue);
-                yield return V(Byte.MaxValue);
-                // SByte
-                yield return V((SByte)0);
-                yield return V(SByte.MinValue);
-                yield return V(SByte.MaxValue);
-                // Int16
-                yield return V((Int16)0);
-                yield return V(Int16.MinValue);
-                yield return V(Int16.MaxValue);
-                // UInt16
-                yield return V((UInt16)0);
-                yield return V(UInt16.MinValue);
-                yield return V(UInt16.MaxValue);
-                // Int32
-                yield return V((Int32)0);
-                yield return V(Int32.MinValue);
-                yield return V(Int32.MaxValue);
-                // UInt32
-                yield return V((UInt32)0);
-                yield return V(UInt32.MinValue);
-                yield return V(UInt32.MaxValue);
-                // Int64
-                yield return V((Int64)0);
-                yield return V(Int64.MinValue);
-                yield return V(Int64.MaxValue);
-                // UInt64
-                yield return V((UInt64)0);
-                yield return V(UInt64.MinValue);
-                yield return V(UInt64.MaxValue);
-                //  Single
-                yield return V((Single)0);
-                yield return V(Single.MinValue);
-                yield return V(Single.MaxValue);
-                yield return V(Single.Epsilon);
-                yield return V(Single.NaN);
-                yield return V(Single.NegativeInfinity);
-                yield return V(Single.PositiveInfinity);
-                // Double
-                yield return V((Double)0);
-                yield return V(Double.MinValue);
-                yield return V(Double.MaxValue);
-                yield return V(Double.Epsilon);
-                yield return V(Double.NaN);
-                yield return V(Double.NegativeInfinity);
-                yield return V(Double.PositiveInfinity);
-                // Decimal
-                yield return V((Decimal)0);
-                yield return V(Decimal.MinValue);
-                yield return V(Decimal.MaxValue);
-                // DBNull
-                yield return V(DBNull.Value);
-                // TimeSpan
-                yield return V(TimeSpan.Zero);
-                yield return V(TimeSpan.MinValue);
-                yield return V(TimeSpan.MaxValue);
-                // DateTime
-                yield return V(DateTime.Now);
-                yield return V(DateTime.MinValue);
-                yield return V(DateTime.MaxValue);
+                yield return A(new EmptyType());
+                yield return A(TypeWithPrivateCtor.New());
+                yield return A(new TypeWithComplexProperties());
+                yield return A(new TypeWithComplexProperties {
+                    EmptyType = new EmptyType(),
+                    TypeWithPrivateCtor = TypeWithPrivateCtor.New()
+                });
             }
 
-            private static object[] V(object value) {
+            private static object[] A(object value) {
 
                 return new object[] { value };
             }
 
-            System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator() {
+            IEnumerator IEnumerable.GetEnumerator() {
+
+                return GetEnumerator();
+            }
+        }
+
+        [TypeId(23409283)]
+        public class EmptyType
+        {
+            public override bool Equals(object obj) {
+
+                return obj != null && obj.GetType() == GetType();
+            }
+
+            public override int GetHashCode() {
+
                 throw new NotImplementedException();
+            }
+        }
+
+        [TypeId(654645645)]
+        public class TypeWithPrivateCtor : EmptyType
+        {
+            private TypeWithPrivateCtor() { }
+
+            public static TypeWithPrivateCtor New() {
+
+                return new TypeWithPrivateCtor();
+            }
+        }
+
+        [TypeId(5528731)]
+        public class TypeWithComplexProperties
+        {
+            [PropertyId(0)]
+            public EmptyType EmptyType { get; set; }
+
+            [PropertyId(1)]
+            public TypeWithPrivateCtor TypeWithPrivateCtor { get; set; }
+
+            public override bool Equals(object obj) {
+
+                if(obj == null || obj.GetType() != GetType()) {
+                    return false;
+                }
+
+                var other = (TypeWithComplexProperties)obj;
+
+                return
+                    object.Equals(other.EmptyType, this.EmptyType) &&
+                    object.Equals(other.TypeWithPrivateCtor, this.TypeWithPrivateCtor);
+            }
+
+            public override int GetHashCode() {
+
+                throw new NotImplementedException();
+            }
+        }
+
+        public class PrimitiveDataProvider : IEnumerable<object[]>
+        {
+            public IEnumerator<object[]> GetEnumerator() {
+
+                // Empty
+                yield return A(null);
+                // String
+                yield return A("s");                
+                // Boolean
+                yield return A(true);
+                yield return A(false);
+                // Char
+                yield return A('c');
+                yield return A(char.MinValue);
+                yield return A(char.MaxValue);
+                // Byte
+                yield return A((Byte)0);
+                yield return A(Byte.MinValue);
+                yield return A(Byte.MaxValue);
+                // SByte
+                yield return A((SByte)0);
+                yield return A(SByte.MinValue);
+                yield return A(SByte.MaxValue);
+                // Int16
+                yield return A((Int16)0);
+                yield return A(Int16.MinValue);
+                yield return A(Int16.MaxValue);
+                // UInt16
+                yield return A((UInt16)0);
+                yield return A(UInt16.MinValue);
+                yield return A(UInt16.MaxValue);
+                // Int32
+                yield return A((Int32)0);
+                yield return A(Int32.MinValue);
+                yield return A(Int32.MaxValue);
+                // UInt32
+                yield return A((UInt32)0);
+                yield return A(UInt32.MinValue);
+                yield return A(UInt32.MaxValue);
+                // Int64
+                yield return A((Int64)0);
+                yield return A(Int64.MinValue);
+                yield return A(Int64.MaxValue);
+                // UInt64
+                yield return A((UInt64)0);
+                yield return A(UInt64.MinValue);
+                yield return A(UInt64.MaxValue);
+                //  Single
+                yield return A((Single)0);
+                yield return A(Single.MinValue);
+                yield return A(Single.MaxValue);
+                yield return A(Single.Epsilon);
+                yield return A(Single.NaN);
+                yield return A(Single.NegativeInfinity);
+                yield return A(Single.PositiveInfinity);
+                // Double
+                yield return A((Double)0);
+                yield return A(Double.MinValue);
+                yield return A(Double.MaxValue);
+                yield return A(Double.Epsilon);
+                yield return A(Double.NaN);
+                yield return A(Double.NegativeInfinity);
+                yield return A(Double.PositiveInfinity);
+                // Decimal
+                yield return A((Decimal)0);
+                yield return A(Decimal.MinValue);
+                yield return A(Decimal.MaxValue);
+                // DBNull
+                yield return A(DBNull.Value);
+                // TimeSpan
+                yield return A(TimeSpan.Zero);
+                yield return A(TimeSpan.MinValue);
+                yield return A(TimeSpan.MaxValue);
+                // DateTime
+                yield return A(DateTime.Now);
+                yield return A(DateTime.MinValue);
+                yield return A(DateTime.MaxValue);
+            }
+
+            private static object[] A(object value) {
+
+                return new object[] { value };
+            }
+
+            IEnumerator IEnumerable.GetEnumerator() {
+                throw new NotImplementedException();
+            }
+        }
+
+        [TypeId(123456789)]
+        private sealed class ObjectReference : IObjectReference
+        {
+            public static readonly ObjectReference Instance = new ObjectReference();
+
+            public object GetRealObject(StreamingContext context) {
+
+                return Instance;
             }
         }
     }
