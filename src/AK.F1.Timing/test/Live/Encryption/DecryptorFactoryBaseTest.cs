57ae1248be0cd7e5a211dc17d816ce94eb79f562
andrew.kernahan <andrew.kernahan@3a0ce762-4747-11df-839d-f73fd85d8354>
2010-03-27 09:58:13 +0000

    Implemented test methods.

--- src/AK.F1.Timing/test/Live/Encryption/DecryptorFactoryBaseTest.cs
+++ src/AK.F1.Timing/test/Live/Encryption/DecryptorFactoryBaseTest.cs
@@ -13,15 +13,93 @@
 // limitations under the License.
 
 using System;
+using Moq;
 using Xunit;
-
-using AK.F1.Timing.Live.Encryption;
 
 namespace AK.F1.Timing.Live.Encryption
 {
-
-
     public class DecryptorFactoryBaseTest
     {
+        [Fact]
+        public void can_create_default_decryptor() {
+
+            var factory = new StubDecryptorFactory();
+
+            Assert.NotNull(factory.Create());
+        }
+
+        [Fact]
+        public void the_default_decryptor_is_created_with_the_default_seed() {
+
+            var factory = new StubDecryptorFactory();
+
+            Assert.NotNull(factory.Create());
+            Assert.Equal(DecryptorFactoryBase.DefaultSeed, factory.CreateWithSeed_Seed);
+        }
+
+        [Fact]
+        public void seed_returned_by_get_seed_for_session_is_used_to_create_the_decryptor() {
+
+            var factory = new StubDecryptorFactory();
+
+            Assert.NotNull(factory.Create("sessionId"));
+            Assert.Equal(7, factory.CreateWithSeed_Seed);
+        }
+
+        [Fact]
+        public void seeds_for_previously_resolved_sessions_are_cached() {
+
+            var factory = new StubDecryptorFactory();
+
+            Assert.NotNull(factory.Create("sessionId"));
+            Assert.Equal(1, factory.GetSeedForSessionCount);
+            Assert.NotNull(factory.Create("sessionId"));
+            Assert.Equal(1, factory.GetSeedForSessionCount);
+        }
+
+        [Fact]
+        public void the_seed_cache_is_case_sensitive() {
+
+            var factory = new StubDecryptorFactory();
+
+            Assert.NotNull(factory.Create("sessionId"));
+            Assert.Equal(1, factory.GetSeedForSessionCount);
+            Assert.NotNull(factory.Create("SessionId"));
+            Assert.Equal(2, factory.GetSeedForSessionCount);
+        }
+
+        [Fact]
+        public void can_get_the_factory_log() {
+
+            var factory = new StubDecryptorFactory();
+
+            Assert.NotNull(factory.GetLogProperty());
+        }
+
+        private sealed class StubDecryptorFactory : DecryptorFactoryBase
+        {
+            protected override int GetSeedForSession(string sessionId) {
+
+                ++this.GetSeedForSessionCount;
+
+                return 7;
+            }
+
+            public int GetSeedForSessionCount { get; set; }
+
+            protected override IDecryptor CreateWithSeed(int seed) {
+
+                this.CreateWithSeed_Seed = seed;
+
+                return new Mock<IDecryptor>().Object;
+            }
+
+            public log4net.ILog GetLogProperty() {
+
+                return this.Log;
+            }
+
+            public int CreateWithSeed_Seed { get; set; }
+        }
     }
 }
