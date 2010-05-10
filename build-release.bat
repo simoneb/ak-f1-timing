916946895b9cd1e711896a97530ba884c094d846
andrew.kernahan <andrew.kernahan@3a0ce762-4747-11df-839d-f73fd85d8354>
2010-05-10 21:06:04 +0000

    Upgraded solutions to 2010. Rebuilt all resources with new tool version. Updated build batch files to invoke msbuild v4.0.30319.

--- build-release.bat
+++ build-release.bat
@@ -3,10 +3,11 @@ if "%1" == "" goto BuildDefault
 goto BuildTarget
 
 :BuildDefault
-%windir%\Microsoft.NET\Framework\v3.5\MSBuild.exe src\AK.F1.Timing.msbuild /p:Configuration=Release;BuildType=Release /t:All
+%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe src\AK.F1.Timing.msbuild /p:Configuration=Release;BuildType=Release /t:All
 goto End
 
 :BuildTarget
-%windir%\Microsoft.NET\Framework\v3.5\MSBuild.exe src\AK.F1.Timing.msbuild /p:Configuration=Release;BuildType=Release /t:%*
+%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe src\AK.F1.Timing.msbuild /p:Configuration=Release;BuildType=Release /t:%*
 
 :End
+l
--- build.bat
+++ build.bat
@@ -3,10 +3,10 @@ if "%1" == "" goto BuildDefault
 goto BuildTarget
 
 :BuildDefault
-%windir%\Microsoft.NET\Framework\v3.5\MSBuild.exe src\AK.F1.Timing.msbuild /p:Configuration=Debug /t:All
+%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe src\AK.F1.Timing.msbuild /p:Configuration=Debug /t:All
 goto End
 
 :BuildTarget
-%windir%\Microsoft.NET\Framework\v3.5\MSBuild.exe src\AK.F1.Timing.msbuild /p:Configuration=Debug /t:%*
+%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe src\AK.F1.Timing.msbuild /p:Configuration=Debug /t:%*
 
 :End
