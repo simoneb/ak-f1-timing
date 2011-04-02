e65a3d33f5fba33e991d7d1ea48108b73474067d
andrew.kernahan <andrew.kernahan@3a0ce762-4747-11df-839d-f73fd85d8354>
2011-04-02 19:10:05 +0000

    Delayed initial commit of SetMinRequiredQuallyTimeMessageTest; should have been commited as part of r784.

---
+++ src/AK.F1.Timing/test/Messages/Session/SetMinRequiredQuallyTimeMessageTest.cs
@@ -0,0 +1,53 @@
+// Copyright 2009 Andy Kernahan
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using Xunit;
+
+namespace AK.F1.Timing.Messages.Session
+{
+    public class SetMinRequiredQuallyTimeMessageTest : MessageTestBase<SetMinRequiredQuallyTimeMessage>
+    {
+        [Fact]
+        public override void can_create()
+        {
+            var message = CreateMessage();
+
+            Assert.Equal(TimeSpan.FromMilliseconds(1D), message.Time);
+        }
+
+        [Fact]
+        public override void can_visit()
+        {
+            var message = CreateMessage();
+            var visitor = CreateMockMessageVisitor();
+
+            visitor.Setup(x => x.Visit(message));
+            message.Accept(visitor.Object);
+            visitor.VerifyAll();
+        }
+
+        [Fact]
+        public void ctor_throws_if_time_is_negative()
+        {
+            Assert.DoesNotThrow(() => { new SetMinRequiredQuallyTimeMessage(TimeSpan.Zero); });
+            Assert.Throws<ArgumentOutOfRangeException>(() => { new SetMinRequiredQuallyTimeMessage(TimeSpan.FromMilliseconds(-1)); });
+        }
+
+        protected override SetMinRequiredQuallyTimeMessage CreateMessage()
+        {
+            return new SetMinRequiredQuallyTimeMessage(TimeSpan.FromMilliseconds(1D));
+        }
+    }
+}
