b363ef39bf6c107cddc6975bde9175b78dacc4e4
andrew.kernahan <andrew.kernahan@3a0ce762-4747-11df-839d-f73fd85d8354>
2011-09-10 22:09:00 +0000

    Corrected root namespace.

--- src/AK.F1.Timing.Server/src/Resource.Designer.cs
+++ src/AK.F1.Timing.Server/src/Resource.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace AK.F1.Timing.Proxy.Server {
+namespace AK.F1.Timing.Server {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace AK.F1.Timing.Proxy.Server {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("AK.F1.Timing.Proxy.Server.Resource", typeof(Resource).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("AK.F1.Timing.Server.Resource", typeof(Resource).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
